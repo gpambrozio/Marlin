8418115df5d7c5e7fd2e56b722c71fbd9c39ae3a
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-10-24 13:16:23 -1000

    Adjusting probing points.
    This makes it stop in the middle/front of my bed for easier nozzle cleaning.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1922,12 +1922,12 @@
  * Override if the automatically selected points are inadequate.
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
-  //#define PROBE_PT_1_X 15
-  //#define PROBE_PT_1_Y 180
-  //#define PROBE_PT_2_X 15
-  //#define PROBE_PT_2_Y 20
-  //#define PROBE_PT_3_X 170
-  //#define PROBE_PT_3_Y 20
+  #define PROBE_PT_1_X (0+PROBING_MARGIN+20)
+  #define PROBE_PT_1_Y (Y_BED_SIZE-PROBING_MARGIN)
+  #define PROBE_PT_2_X (X_BED_SIZE-PROBING_MARGIN)
+  #define PROBE_PT_2_Y (Y_BED_SIZE-PROBING_MARGIN)
+  #define PROBE_PT_3_X (X_BED_SIZE/2)
+  #define PROBE_PT_3_Y (0+PROBING_MARGIN)
 #endif
 
 /**
